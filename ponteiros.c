d2ac1d41077c32bb592f894e59e5661eee227916
Pedro <phlmesquita@inf.ufpel.edu.br>
2023-07-22 23:43:04 -0300

    pointers 2.0

--- ponteiros.c
+++ ponteiros.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 
-void calcula(int c){
-    printf("calcula %d\n", c);
-    c++;
-    printf("calcula %d\n", c);
+void calcula(int* c){
+    printf("calcula %d %d\n", (*c), c);
+    (*c)++;
+    printf("calcula %d %d\n", (*c), c);
+
 }
 
 int main(){
 
     int c = 10;
 
-    printf("main %d\n", c);
-    calcula(c);
+    printf("main %d %d\n", c, &c);
+    calcula(&c);
 
-    printf("main %d\n", c);
+    printf("main %d %d\n", c, &c);
 
 }

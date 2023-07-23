722392249a8689629e392837af299b6a4de89ff6
Pedro <phlmesquita@inf.ufpel.edu.br>
2023-07-22 22:59:06 -0300

    funções

--- forca.c
+++ forca.c
@@ -2,29 +2,64 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
+void abertura(){
+    printf("/****************/\n");
+    printf("/ Jogo de Forca */\n");
+    printf("/****************/\n\n");
+}
+//valores 
+void chuta(char chutes[], int tentativas){
+    
+    char chute;
+    printf("Qual é a letra? \n");
+    scanf(" %c", &chute);       
+
+    chutes[tentativas] = chute;
+    }
+
 int main(){
+
+    abertura();
+
     char palavrasecreta[20];
 
     sprintf(palavrasecreta,"MELANCIA");
+
     int acertou = 0;
     int enforcou = 0;
-
-    char chutes[26];
     int tentativas = 0;
-
+    
+    char chutes[26];
+    
     do {
 
         for (int i = 0; i < strlen(palavrasecreta); i++){
-            printf("_ ");
             
+            int achou = 0;
+            
+            // printf("Estou vendo a palavra secreta %d = %c\n",i , palavrasecreta[i]);
+            for (int j = 0; j < tentativas; j++){
+               // printf("------>>Chute %d = %c\n", j , chutes[j]);
+                if (chutes[j] == palavrasecreta[i]){
+                 //printf("--------->>Chute correto!!!\n");
+                 achou = 1;
+                 break;   
+                }
+            }
+            
+            if (achou){
+                printf("%c ", palavrasecreta[i]);
+            }else{
+                printf("_ ");
+            }
         }
         printf("\n\n");
-        
-        char chute;
-        scanf(" %c", &chute);       
 
-        chutes[tentativas] = chute;
+        //passa o valor das variáveis pra minha função chuta
+        chuta(chutes, tentativas);
         tentativas++;
-        
+
         }while(!acertou && !enforcou);
 }
